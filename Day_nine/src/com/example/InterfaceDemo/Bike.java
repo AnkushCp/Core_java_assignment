12005cef63221c9edde6adf0e783dd0132027f52
Ankush Gautam Chourpagar <111242504+AnkushCp@users.noreply.github.com>
2023-10-13 00:44:51 +0530

    Delete Day_nine directory

--- Day_nine/src/com/example/InterfaceDemo/Bike.java
+++
@@ -1,9 +0,0 @@
-package com.example.InterfaceDemo;
-
-public class Bike {
-	public int cal(int a)
-	{
-		a=a+10;
-		return a;
-	}
-}
--- Day_nine/src/com/example/InterfaceDemo/MakeRoti.java
+++
@@ -1,8 +0,0 @@
-package com.example.InterfaceDemo;
-
-public interface MakeRoti {
-	String ingri2="salt";
-	String ingri3="water";
-	void display();
-
-}
